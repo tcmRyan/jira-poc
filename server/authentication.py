b9594c8e3b5cb202216808a26d305240a67ba76e
Ryan Schaffer <ryan@tacticalcodemonkeys.com>
2017-08-19 18:13:12 -0400

    stuck on jwt

--- server/authentication.py
+++ server/authentication.py
@@ -1,62 +1,11 @@
-import time
-from server.models import Authentication
-import jwt
-from urllib import parse
-from collections import OrderedDict
-import hashlib
+import atlassian_jwt
 
 
-class JWTToken(object):
+class JWTToken(atlassian_jwt.Authenticator):
 
-    header = {
-        "typ": "JWT",
-        "alg": "HS256"
-    }
+    def __init__(self, tenant_info_store):
+        super(JWTToken, self).__init__()
+        self.tenant_info_store = tenant_info_store
 
-    def __init__(self):
-        self.auth = Authentication.query.get(2)
-        self.shared_secret = self.auth.shared_secret
-        self.base_url = self.auth.base_url
-        self.context_path = '/'
-        self.method = 'GET'
-        self.api_path = '/rest/atlassian-connect/latest/license'
-        self.jwt = None
-        self.create_uri_with_jwt()
-
-    def create_uri_with_jwt(self):
-        # TODO: Make this real, right now just using the data in the test db
-
-        issued_at = int(time.time())
-        expires_at = issued_at + (60 * 3)  # 3 min window
-        claim = {
-            'iss': self.auth.key,
-            'iat': issued_at,
-            'exp': expires_at,
-            'qsh': self.get_qsh(self.method, self.api_path)
-        }
-
-        self.jwt = jwt.encode(claim, self.shared_secret, algorithm='HS256', headers=self.header)
-
-    def get_qsh(self, method, api_path, params={}, data={}):
-        method = method.upper()
-        canonical_uri = api_path if api_path else '/'
-        if method == 'GET':
-            params.pop('jwt', None)
-            claims_data = self.sort_data(params)
-        elif method == 'POST':
-            claims_data = self.sort_data(data)
-        else:
-            claims_data = ''
-        canonical_qs = parse.urlencode(claims_data, encoding='utf-8')
-        canonical = '&'.join([method, canonical_uri, canonical_qs])
-        return hashlib.sha256(canonical.encode('utf-8')).hexdigest()
-
-    def sort_data(self, data_or_params):
-        # TODO Atlassian recommends that you sort after encoding, ignoring that requirement for now
-        return OrderedDict(sorted(data_or_params.items()))
-
-
-
-
-
-
+    def get_shared_secret(self, client_key):
+        return self.tenant_info_store['sharedSecret']
--- server/views.py
+++ server/views.py
@@ -3,7 +3,7 @@ from server import app, db
 from flask import render_template, send_from_directory, request
 import requests
 from server.models import Authentication
-from server.authentication import JWTToken
+import atlassian_jwt
 
 site_root = os.path.realpath(os.path.dirname(__file__))
 static_path = os.path.join(os.path.join(site_root, "static"))
@@ -13,10 +13,15 @@ template_path = os.path.join(os.path.join(site_root, "templates"))
 @app.route('/hello-world')
 def home():
     # return send_from_directory(template_path, 'hello_world.html')
-    token = JWTToken()
-    headers = {'Authorization': 'JWT {}'.format(token.jwt)}
-    resp = requests.get(token.base_url + token.api_path, headers=headers, params={'jwt': token.jwt})
-    return render_template('hello_world.html')
+    tenat_info = Authentication.query.get(4).__dict__
+    tenat_info.pop('_sa_instance_state')
+    url = tenat_info['baseUrl'] + '/rest/api/latest/serverInfo'
+    token = atlassian_jwt.encode_token('GET', url, **tenat_info)
+    headers = {'Authorization': 'JWT {}'.format(token)}
+
+    resp = requests.get(url, headers=headers)
+    print(resp.content)
+    return resp.content
 
 
 @app.route('/install/<descriptor>')
