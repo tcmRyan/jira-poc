90356e8d39ae61f675890510b075fda1d0f4a855
Ryan Schaffer <ryan@tacticalcodemonkeys.com>
2017-08-25 18:50:14 -0400

    JWT working locally with authentication library

--- server/authentication.py
+++ server/authentication.py
@@ -1,11 +1,51 @@
 import atlassian_jwt
+import requests
+from urllib.parse import urlencode
 
 
-class JWTToken(atlassian_jwt.Authenticator):
+class SimpleAuthenticator(atlassian_jwt.Authenticator):
 
     def __init__(self, tenant_info_store):
-        super(JWTToken, self).__init__()
+        super(SimpleAuthenticator, self).__init__()
         self.tenant_info_store = tenant_info_store
 
     def get_shared_secret(self, client_key):
         return self.tenant_info_store['sharedSecret']
+
+
+class AtlassianRequest(object):
+
+    session = None
+    responses = None
+
+    def __init__(self, tenant_data):
+        self.session = requests.Session()
+        self.tenant_data = tenant_data
+
+    def _authorize(self, uri, method):
+        jwt_auth = atlassian_jwt.encode_token(
+            method,
+            uri,
+            self.tenant_data['key'],
+            self.tenant_data['sharedSecret']
+        )
+        return {'Authorization': 'JWT {}'.format(jwt_auth)}
+
+    def request(self, uri, method='GET', **kwargs):
+        request_args = {
+            'url': uri,
+            'method': method
+        }
+        if kwargs.get('params'):
+            uri = uri + urlencode(kwargs.get('params'))
+
+        auth_header = self._authorize(uri, method)
+        headers = kwargs.get('headers', {})
+        kwargs['headers'] = auth_header if not headers else headers.append(auth_header)
+
+        request_args.update(kwargs)
+
+        return self.session.request(**request_args)
+
+    def reset(self):
+        self.session = requests.Session()
--- server/views.py
+++ server/views.py
@@ -1,9 +1,8 @@
 import os
 from server import app, db
 from flask import render_template, send_from_directory, request
-import requests
 from server.models import Authentication
-import atlassian_jwt
+from server.authentication import AtlassianRequest
 
 site_root = os.path.realpath(os.path.dirname(__file__))
 static_path = os.path.join(os.path.join(site_root, "static"))
@@ -13,13 +12,16 @@ template_path = os.path.join(os.path.join(site_root, "templates"))
 @app.route('/hello-world')
 def home():
     # return send_from_directory(template_path, 'hello_world.html')
-    tenat_info = Authentication.query.get(4).__dict__
+    tenat_info = Authentication.query.get(9).__dict__
     tenat_info.pop('_sa_instance_state')
+    # url = tenat_info['baseUrl'] + '/rest/atlassian-connect/1/addons/{}'.format(tenat_info['key'])
     url = tenat_info['baseUrl'] + '/rest/api/latest/serverInfo'
-    token = atlassian_jwt.encode_token('GET', url, **tenat_info)
-    headers = {'Authorization': 'JWT {}'.format(token)}
-
-    resp = requests.get(url, headers=headers)
+    # token = atlassian_jwt.encode_token('GET', url, tenat_info['key'], tenat_info['sharedSecret'])
+    # headers = {'Authorization': 'JWT {}'.format(token)}
+    #
+    # resp = requests.get(url, headers=headers)
+    requester = AtlassianRequest(tenat_info)
+    resp = requester.request(url)
     print(resp.content)
     return resp.content
 
