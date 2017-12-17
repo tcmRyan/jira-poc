d02a65b15f55747e1011f33801281a8ff2a1f403
Ryan Schaffer <ryan@tacticalcodemonkeys.com>
2017-12-17 16:38:17 -0500

    Clarity on what multiple endpoints would look like

--- server/webhooks/callbacks.py
+++ server/webhooks/callbacks.py
@@ -5,7 +5,7 @@ from server import app
 
 class IssueCreationCallback(WebHook):
     @app.route('/callbacks/issue-creation')
-    def callback(self):
+    def callbacks_issue_creation(self):
         data = request.get_json()
         ctx = WebhookContext(data['issue'])
         print('BaseUrl: {}, Key: {}, SharedSecret: {}'.format(ctx.base_url, ctx.key, ctx.shared_secret))
@@ -15,10 +15,27 @@ class IssueCreationCallback(WebHook):
     def descriptor(self):
         description = {
             "event": "jira:issue_created",
-            "url": url_for('callback'),
+            "url": url_for('callbacks_issue_creation'),
             "excludeBody": False,
             "filter": "",
         }
         return description
 
 
+class IssueCreationCallback2(WebHook):
+    @app.route('/callbacks/issue-something-else')
+    def callbacks_issue_something_else(self):
+        data = request.get_json()
+        ctx = WebhookContext(data['issue'])
+        print('BaseUrl: {}, Key: {}, SharedSecret: {}'.format(ctx.base_url, ctx.key, ctx.shared_secret))
+        return 'OK'
+
+    @property
+    def descriptor(self):
+        description = {
+            "event": "jira:issue_created",
+            "url": url_for('callbacks_issue_something_else'),
+            "excludeBody": False,
+            "filter": "",
+        }
+        return description
